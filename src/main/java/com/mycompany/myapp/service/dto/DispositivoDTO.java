f303327207590814f0daf854688efb41df836b0b
Rodrigo Alvarez <rorototi@hotmail.com>
2024-11-28 21:02:25 -0300

    funcionando

--- src/main/java/com/mycompany/myapp/service/dto/DispositivoDTO.java
+++ src/main/java/com/mycompany/myapp/service/dto/DispositivoDTO.java
@@ -3,6 +3,7 @@ package com.mycompany.myapp.service.dto;
 import com.mycompany.myapp.domain.enumeration.Moneda;
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -24,6 +25,34 @@ public class DispositivoDTO implements Serializable {
     private Moneda moneda;
 
     private Long idCatedra;
+
+    private List<CaracteristicaDTO> caracteristicas;
+    private List<PersonalizacionDTO> personalizaciones;
+    private List<AdicionalDTO> adicionales;
+
+    public List<CaracteristicaDTO> getCaracteristicas() {
+        return caracteristicas;
+    }
+
+    public void setCaracteristicas(List<CaracteristicaDTO> caracteristicas) {
+        this.caracteristicas = caracteristicas;
+    }
+
+    public List<PersonalizacionDTO> getPersonalizaciones() {
+        return personalizaciones;
+    }
+
+    public void setPersonalizaciones(List<PersonalizacionDTO> personalizaciones) {
+        this.personalizaciones = personalizaciones;
+    }
+
+    public List<AdicionalDTO> getAdicionales() {
+        return adicionales;
+    }
+
+    public void setAdicionales(List<AdicionalDTO> adicionales) {
+        this.adicionales = adicionales;
+    }
 
     public Long getId() {
         return id;
--- src/main/java/com/mycompany/myapp/service/mapper/DispositivoMapper.java
+++ src/main/java/com/mycompany/myapp/service/mapper/DispositivoMapper.java
@@ -7,5 +7,15 @@ import org.mapstruct.*;
 /**
  * Mapper for the entity {@link Dispositivo} and its DTO {@link DispositivoDTO}.
  */
-@Mapper(componentModel = "spring")
-public interface DispositivoMapper extends EntityMapper<DispositivoDTO, Dispositivo> {}
+@Mapper(componentModel = "spring", uses = { CaracteristicaMapper.class, PersonalizacionMapper.class, AdicionalMapper.class })
+public interface DispositivoMapper extends EntityMapper<DispositivoDTO, Dispositivo> {
+    @Mapping(source = "caracteristicas", target = "caracteristicas")
+    @Mapping(source = "personalizacions", target = "personalizaciones")
+    @Mapping(source = "adicionals", target = "adicionales")
+    DispositivoDTO toDto(Dispositivo dispositivo);
+
+    @Mapping(source = "caracteristicas", target = "caracteristicas")
+    @Mapping(source = "personalizaciones", target = "personalizacions")
+    @Mapping(source = "adicionales", target = "adicionals")
+    Dispositivo toEntity(DispositivoDTO dispositivoDTO);
+}
